778970e5386aa46bdf4c7375bfb842ff7339cb85
she110ff <youngsoo.j@gmail.com>
2023-08-25 15:40:31 +0900

    add multisig aa test code

--- contracts/test/utils/sendtx.ts
+++ contracts/test/utils/sendtx.ts
@@ -33,6 +33,50 @@ export async function sendTx(provider: Provider, account: Contract, user: Wallet
         sentTx = await provider.sendTransaction(utils.serialize(tx));
         await sentTx.wait();
     } catch (e) {
+        console.log('e:', e)
+        sentTx = function () { throw e; };
+    }
+
+    return sentTx
+}
+
+
+export async function sendTxForMultisig(provider: Provider, account: Contract, user1: Wallet, user2: Wallet, tx: any) {
+    tx = {
+        ...tx,
+        from: account.address,
+        chainId: (await provider.getNetwork()).chainId,
+        nonce: await provider.getTransactionCount(account.address),
+        type: 113,
+        customData: {
+            gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
+        } as types.Eip712Meta,
+    }
+
+    tx.gasPrice = await provider.getGasPrice()
+    if (tx.gasLimit == undefined) {
+        tx.gasLimit = ethers.BigNumber.from(20000000)
+    }
+
+    const signedTxHash = EIP712Signer.getSignedDigest(tx)
+    const signature = ethers.utils.concat([
+        // Note, that `signMessage` wouldn't work here, since we don't want
+        // the signed hash to be prefixed with `\x19Ethereum Signed Message:\n`
+        ethers.utils.joinSignature(user1._signingKey().signDigest(signedTxHash)),
+        ethers.utils.joinSignature(user2._signingKey().signDigest(signedTxHash)),
+    ]);
+
+    tx.customData = {
+        ...tx.customData,
+        customSignature: signature,
+    }
+
+    let sentTx ;
+    try{
+        sentTx = await provider.sendTransaction(utils.serialize(tx));
+        await sentTx.wait();
+    } catch (e) {
+        console.log('e:', e)
         sentTx = function () { throw e; };
     }
 
